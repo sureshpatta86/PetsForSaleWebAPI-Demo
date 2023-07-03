6fc29135a4718b81729a93f90ea90ab3b2f414d3
Suresh Patta <58070484+sureshpatta86@users.noreply.github.com>
2023-07-04 00:28:53 +0530

    Update Program.cs

--- PetsForSaleWebAPI/Program.cs
+++ PetsForSaleWebAPI/Program.cs
@@ -1,11 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Add services to the container.
 builder.Services.AddDbContext<EFDataContext>(t => t.UseNpgsql(builder.Configuration.GetConnectionString("azure_Postgres_Db")));
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 
